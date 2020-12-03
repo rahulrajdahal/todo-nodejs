eee8da2e309ec1bfc9722f2b66e975fc00494415
Markaa10 <rahulrajdahal@gmail.com>
2020-12-03 17:07:37 +0545

    user model

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password cannot be password");
+      }
+    },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
